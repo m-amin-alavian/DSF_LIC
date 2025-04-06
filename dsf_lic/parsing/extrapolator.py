e3173d2b7dc43158ab72ea40d9d89b452a77a2a5
mohammad amin alavian <m.amin.alavian@gmail.com>
2025-04-06 17:47:14 +0330

    Add parameters to extrapolate

--- dsf_lic/parsing/extrapolator.py
+++ dsf_lic/parsing/extrapolator.py
@@ -2,10 +2,14 @@ from typing import Optional, NamedTuple, Literal
 
 import pandas as pd
 
+from ..utils.metadata import metadata
+
 
 class ExtrapolateOptions(NamedTuple):
     method: Literal["arithmetic", "geometric"] = "arithmetic"
     number_of_periods: int = 5
+    last_year: Optional[int | Literal["projection_year"]] = None
+    offset: int = 0
 
 
 def extrapolate(
@@ -18,9 +22,19 @@ def extrapolate(
     parameters = {} if parameters is True else parameters
     options = ExtrapolateOptions(**parameters)
 
+    if options.last_year is None:
+        last_year = column.loc[lambda s: s.notna()].index[-1]
+    if options.last_year == "projection_year":
+        last_year = metadata.setting.projection_year
+    else:
+        last_year = options.last_year
+    last_value = column.loc[last_year]
+
+
     if options.method == "arithmetic":
         growth_average = (
             column
+            .loc[:last_year-options.offset]
             .div(column.shift(1))
             .sub(1)
             .loc[lambda s: s.notna()]
@@ -30,9 +44,6 @@ def extrapolate(
     else:
         raise NotImplementedError
 
-    last_year = column.loc[lambda s: s.notna()].index[-1]
-    last_value = column.loc[last_year]
-
     extrapolated_values = (
         pd.Series(
             growth_average,
@@ -40,6 +51,7 @@ def extrapolate(
         )
         .add(1).cumprod().mul(last_value)
     )
+    column.loc[last_year:] = None
     column = column.fillna(extrapolated_values)
 
     return column
